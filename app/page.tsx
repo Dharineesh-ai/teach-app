2c48ff920b1096a3457c4111dc17a58d804466e0
Dharineesh-ai <dharineeshv.21msc@kongu.edu>
2025-06-08 13:56:05 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Welcome  to SaaS Application</h1>
+      <Button>Let's Get Started</Button>
+    </div>
+  )
+}
+
+export default Page
